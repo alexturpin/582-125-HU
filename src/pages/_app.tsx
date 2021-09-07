e2315905268d00992cff51124a40a8d38dcb1b10
Alex Turpin <alex.turpin@gmail.com>
2021-09-07 12:39:52 -0400

    Home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
-import "tailwindcss/tailwind.css"
+import "../styles/globals.css"
 import type { AppProps } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto py-4">
+      <h1 className="text-3xl font-bold">Intégration multimédia 1</h1>
+      <h4 className="text-2xl font-semibold mb-6">582-125-HU</h4>
+
       <Component {...pageProps} />
     </div>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,44 @@
 export default function Home() {
-  return <p>Home</p>
+  return (
+    <>
+      <section>
+        <h3>Détails du cours</h3>
+        <ul>
+          <li>
+            Alexandre Turpin (chargé de cours) -{" "}
+            <a href="mailto:alex.turpin@gmail.com">alex.turpin@gmail.com</a>
+          </li>
+          <li>
+            <a href="/files/582-125-HU_PC.pdf">Plan de cours</a>
+          </li>
+          <li>
+            <a href="/files/horaire.pdf">Horaire</a>
+          </li>
+        </ul>
+      </section>
+
+      <section>
+        <h3>Liens utiles</h3>
+        <ul>
+          <li>
+            <a href="https://developer.mozilla.org/en-US/">MDN</a>
+            <ul>
+              <li>
+                <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element">
+                  Référence des éléments HTML
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="https://code.visualstudio.com/">Visual Studio Code</a>
+          </li>
+        </ul>
+      </section>
+
+      <section>
+        <h3>Notes de cours</h3>
+      </section>
+    </>
+  )
 }

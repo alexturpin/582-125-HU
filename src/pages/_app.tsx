072a4eb0cb628c6070024c909c733dc1690e1d45
Alex Turpin <alex.turpin@gmail.com>
2021-09-07 16:54:34 -0400

    First course

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,21 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="container mx-auto py-4">
-      <h1 className="text-3xl font-bold">Intégration multimédia 1</h1>
-      <h4 className="text-2xl font-semibold mb-6">582-125-HU</h4>
+    <>
+      <Head>
+        <title>Intégration multimédia 1</title>
+      </Head>
 
-      <Component {...pageProps} />
-    </div>
+      <div className="container mx-auto py-4">
+        <h1 className="text-3xl font-bold">Intégration multimédia 1</h1>
+        <h4 className="text-2xl font-semibold mb-6">582-125-HU</h4>
+
+        <Component {...pageProps} />
+      </div>
+    </>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
-export default function Home() {
+import Link from "next/link"
+
+const Home = () => {
   return (
     <>
       <section>
-        <h3>Détails du cours</h3>
+        <h3>Détails sur le cours</h3>
         <ul>
           <li>
             Alexandre Turpin (chargé de cours) -{" "}
@@ -31,14 +33,34 @@ export default function Home() {
             </ul>
           </li>
           <li>
+            <a href="https://www.w3schools.com/">W3Schools</a>
+          </li>
+          <li>
             <a href="https://code.visualstudio.com/">Visual Studio Code</a>
+          </li>
+          <li>
+            <a href="https://loremipsum.io/">
+              Générateur de <em>Lorem Ipsum</em> (faux texte)
+            </a>
+          </li>
+          <li>
+            <a href="https://validator.w3.org/#validate_by_input">Validateur HTML du W3C</a>
           </li>
         </ul>
       </section>
 
       <section>
         <h3>Notes de cours</h3>
+        <ul>
+          <li>
+            <Link href="/cours/01-intro-html">
+              <a>Semaine 1 - Introduction au web et au HTML</a>
+            </Link>
+          </li>
+        </ul>
       </section>
     </>
   )
 }
+
+export default Home
